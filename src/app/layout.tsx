814e7519ac2721deed50e17957fd90a5c8efa85c
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-07 15:15:30 +0400

    Footer component and home page is finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import { Recursive } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
+const recursive = Recursive({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,11 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${recursive.className} antialiased`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
