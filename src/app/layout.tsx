b99e2ba44304a60347cfce5f49358a76bc4c090d
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-07 15:52:36 +0400

    on configure/upload route uploading is almost finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang='en'>
       <body className={`${recursive.className} antialiased`}>
         <Navbar />
-        {children}
+        <main className='flex flex-col min-h-[calc(100vh-8.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>{children}</div>
+        </main>
         <Footer />
       </body>
     </html>
