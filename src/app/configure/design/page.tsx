38c98254ddd3f83a5f4bf766177f93bec3ed5a71
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-07 20:58:20 +0400

    upload logic is done prisma configuration model and its managment but we have problem returning created/updated configuration id on the core.ts

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,12 @@
-const Page = async () => {
-  return <div>Page</div>
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = await searchParams
+  return <div>{id}</div>
 }
 
 export default Page
