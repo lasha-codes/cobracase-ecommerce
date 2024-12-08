6a0f092af45e22e5b2fe6a4d452b08ae7dde1543
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-08 23:57:33 +0400

    resizing image logic is done with the react-rdn package

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import HandleComponent from '@/components/HandleComponent'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { cn } from '@/lib/utils'
+import { ScrollArea } from '@radix-ui/react-scroll-area'
 import NextImage from 'next/image'
+import { Rnd } from 'react-rnd'
+import { RadioGroup } from '@headlessui/react'
+import { useState } from 'react'
+import { COLORS } from '@/validators/option-validator'
 
 interface DesignConfiguratorProps {
   configId: string
@@ -16,6 +24,12 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number]
+  }>({
+    color: COLORS[0],
+  })
+
   return (
     <div className='relative mt-20 grid grid-cols-3 mb-20 pb-20'>
       <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none ring-2 focus:ring-2 focus:ring-primary focus:ring-offset-2'>
@@ -35,10 +49,65 @@ const DesignConfigurator = ({
           <div
             className={cn(
               'absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]',
-              `bg-blue-950`
+              `bg-zinc-950`
             )}
           />
         </div>
+
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          lockAspectRatio
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+            topLeft: <HandleComponent />,
+          }}
+          className='absolute z-20 border-[2px] border-primary'
+        >
+          <div className='relative w-full h-full'>
+            <NextImage
+              src={imageUrl}
+              fill
+              alt='your image'
+              className='pointer-events-none'
+            />
+          </div>
+        </Rnd>
+      </div>
+
+      <div className='h-[37.5rem] flex flex-col bg-white'>
+        <ScrollArea className='relative flex-1 overflow-auto'>
+          <div
+            aria-hidden='true'
+            className='absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none'
+          />
+
+          <div className='px-8 pb-12 pt-8'>
+            <h2 className='tracking-tight font-bold text-3xl'>
+              Customize your case
+            </h2>
+
+            <div className='w-full h-px bg-zinc-200 my-6' />
+
+            <div className='relative mt-4 h-full flex flex-col justify-between'>
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: val,
+                  }))
+                }}
+              ></RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   )
