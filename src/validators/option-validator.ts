d714ce320a748de084da082303e973343d86671e
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-09 00:44:21 +0400

    now we can select our iphone model with the beautifull dropdown

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -7,3 +7,37 @@ export const COLORS = [
   { label: 'Blue', value: 'blue', tw: 'blue-950' },
   { label: 'Rose', value: 'rose', tw: 'rose-950' },
 ] as const
+
+export const MODELS = {
+  name: 'models',
+  options: [
+    {
+      label: 'Iphone X',
+      value: 'iphonex',
+    },
+    {
+      label: 'Iphone 11',
+      value: 'iphone11',
+    },
+    {
+      label: 'Iphone 12',
+      value: 'iphone12',
+    },
+    {
+      label: 'Iphone 13',
+      value: 'iphone13',
+    },
+    {
+      label: 'Iphone 14',
+      value: 'iphone14',
+    },
+    {
+      label: 'Iphone 15',
+      value: 'iphone15',
+    },
+    {
+      label: 'Iphone 16',
+      value: 'iphone16',
+    },
+  ],
+} as const
